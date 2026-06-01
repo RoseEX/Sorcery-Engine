3d3def5c5fbe84fe6c95d515b91e81b3eb4a0474
Rose EX <gabrielmaxx07pro@gmail.com>
2026-06-01 17:42:03 +0100

    changed domain clash text to red (supposed)

--- Intersect.Server.Core/Maps/DomainExpansionManager.cs
+++ Intersect.Server.Core/Maps/DomainExpansionManager.cs
@@ -63,8 +63,8 @@ public static class DomainExpansionManager
             // If they are clashing (equal power), neutralize sure-hits
             existing.Caster.IsSureHitNeutralized = true;
             challenger.IsSureHitNeutralized = true;
-            PacketSender.SendActionMsg(challenger, "DOMAIN CLASH: Sure-hit neutralized!", CustomColors.Combat.Status);
-            PacketSender.SendActionMsg(existing.Caster, "DOMAIN CLASH: Sure-hit neutralized!", CustomColors.Combat.Status);
+            PacketSender.SendActionMsg(challenger, "DOMAIN CLASH: Sure-hit neutralized!", CustomColors.Combat.Critical);
+            PacketSender.SendActionMsg(existing.Caster, "DOMAIN CLASH: Sure-hit neutralized!", CustomColors.Combat.Critical);
         }
     }
 
